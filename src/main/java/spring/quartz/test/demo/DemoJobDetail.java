d4ea554bf249762f8fbb810ce045aacf96d9ad2d
wangxiaohu <wangxiaohu@ykse.com.cn>
2017-08-29 16:57:49 +0800

    no message

--- src/main/java/spring/quartz/test/demo/DemoJobDetail.java
+++ src/main/java/spring/quartz/test/demo/DemoJobDetail.java
@@ -4,15 +4,27 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-
+/**
+ * demo job
+ * 
+ * @author wangxiaohu
+ * @version $Id: DemoJobDetail.java, v 0.1 2017年8月29日 下午1:50:54 wangxiaohu Exp $
+ */
 public class DemoJobDetail extends QuartzJobBean{
-    //这里就是因为有上文中的AutowiringSpringBeanJobFactory才可以使用@Autowired注解，否则只能在配置文件中设置这属性的值
+    //这里就是因为有上文中的AutowiredInternalBeanOfJobFactory才可以使用@Autowired注解
     @Autowired
     private DemoService demoService;
+    
+    //可以通过配置文件的中JobDetailFactoryBean里面的jobDataAsMap注入，但是需要提供set方法
+    private String userName;
 
     @Override
-    protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("DemoJobDetail.run..");
         demoService.printNowTime();
     }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
